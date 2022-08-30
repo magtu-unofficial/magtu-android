9d770b94c315c656e4569673b92f3a78c33d399b
Eremey Malinovsky <Elerphore@gmail.com>
2022-08-30 07:57:20 +0500

    feat(): Setting screen

--- app/src/main/java/ru/elerphore/magtu_android/ui/screens/setting_screen/SettingScreen.kt
+++ app/src/main/java/ru/elerphore/magtu_android/ui/screens/setting_screen/SettingScreen.kt
@@ -1,27 +1,36 @@
 package ru.elerphore.magtu_android.ui.screens.setting_screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun SettingScreen() {
-    Column() {
-        var textOne by remember { mutableStateOf("Hello") }
-        var textTwo by remember { mutableStateOf("Hello") }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        var groupName by remember { mutableStateOf("") }
 
         TextField(
-            value = textOne,
-            onValueChange = { textOne = it },
-            label = { Text("Label") }
+            value = groupName,
+            singleLine = true,
+            onValueChange = { groupName = it },
+            placeholder = { Text(text = "Название группы") },
+            label = { Text("Введите вашу группу") }
         )
-        TextField(
-            value = textTwo,
-            onValueChange = { textTwo = it },
-            label = { Text("Label") }
-        )
+
         Button(onClick = {
 
         }) {
