6315e377ce7c6b48651bc467cde9fbf385327660
Eremey Malinovsky <Elerphore@gmail.com>
2022-08-30 07:42:19 +0500

    feat(): Database as an object

--- app/src/main/java/ru/elerphore/magtu_android/MainActivity.kt
+++ app/src/main/java/ru/elerphore/magtu_android/MainActivity.kt
@@ -6,22 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
-import androidx.room.Room
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
-import ru.elerphore.magtu_android.data.Database
-import ru.elerphore.magtu_android.data.Settings
 import ru.elerphore.magtu_android.http_client.WebClient
-import ru.elerphore.magtu_android.ui.BottomNavigation
 import ru.elerphore.magtu_android.ui.Main
-import ru.elerphore.magtu_android.ui.screens.NavigationGraph
 
 val wc = WebClient()
 
@@ -36,30 +27,12 @@ val paddingModifier  = Modifier
     }
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        CoroutineScope(IO).launch {
-
-            val db = Room.databaseBuilder(
-                applicationContext,
-                Database::class.java, "magtu"
-            ).build()
-
-            val settingsDao = db.settingsDao()
-            val list = settingsDao.getAll()
-
-            if(list.isEmpty()) {
-                settingsDao.insert(Settings(0, "ИСп-19-4", false))
-            }
-
-            println("LIST DATABASE: $list")
+        setContent {
+            Main()
         }
-
-            setContent {
-                Main()
-            }
     }
 }
 
---
+++ app/src/main/java/ru/elerphore/magtu_android/data/MagtuDatabase.kt
@@ -0,0 +1,22 @@
+package ru.elerphore.magtu_android.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Settings::class], version = 1)
+abstract class MagtuDatabase : RoomDatabase() {
+    abstract fun settingsDao(): SettingsDao
+}
+
+object DB {
+    lateinit var db: MagtuDatabase
+
+    lateinit var appContext: Context
+
+    fun provide(context: Context) {
+        appContext = context
+        db = Room.databaseBuilder(context, MagtuDatabase::class.java, "magtu.db").build()
+    }
+}
